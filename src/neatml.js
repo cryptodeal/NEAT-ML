bdc59be5376763502ba4903cd38ed364a40fe8f9
cryptodeal <jimmydeal16@gmail.com>
2021-07-11 18:39:14 -0500

    finish porting files; update license; webpack5 build fails

---
+++ src/neatml.js
@@ -0,0 +1,39 @@
+const NeatML = {
+	methods: require('./methods/methods'),
+	Connection: require('./architecture/connection'),
+	architect: require('./architecture/architect'),
+	Network: require('./architecture/network'),
+	config: require('./config'),
+	Group: require('./architecture/group'),
+	Layer: require('./architecture/layer'),
+	Node: require('./architecture/node'),
+	Neat: require('./neat'),
+	multi: require('./multithreading/multi')
+};
+
+/* CommonJS & AMD */
+/* eslint-disable no-undef */
+if (typeof define !== 'undefined' && define.amd) {
+	define([], function () {
+		return NeatML;
+	});
+}
+/* eslint-enable no-undef */
+
+/* Node.js */
+if (typeof module !== 'undefined' && module.exports) {
+	module.exports = NeatML;
+}
+
+/* Browser */
+if (typeof window === 'object') {
+	(function () {
+		var old = window['neatml'];
+		NeatML.ninja = function () {
+			window['neatml'] = old;
+			return NeatML;
+		};
+	})();
+
+	window['neatml'] = NeatML;
+}
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+/* Import */
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+/* Update readme and read license */
+var version = require('./package.json').version;
+var readme = fs
+	.readFileSync('./README.md', 'utf-8')
+	.replace(/cdn\/(.*)\/neatml.js/, `cdn/${version}/neatml.js`);
+fs.writeFileSync('./README.md', readme);
+
+var license = fs.readFileSync('./LICENSE', 'utf-8');
+
+/* Export config */
+module.exports = {
+	mode: 'production',
+	context: __dirname,
+	entry: {
+		'dist/neatml': './src/neatml.js',
+		[`mkdocs/theme/cdn/${version}/neatml`]: './src/neatml.js'
+	},
+	resolve: {
+		modules: [path.join(__dirname, 'node_modules')],
+		fallback: {
+			assert: require.resolve('assert'),
+			util: require.resolve('util'),
+			console: require.resolve('console-browserify')
+		}
+	},
+	output: {
+		path: __dirname,
+		filename: '[name].js',
+		library: 'NEAT-ML',
+		libraryTarget: 'umd'
+	},
+	plugins: [
+		new webpack.BannerPlugin(license),
+		new CopyWebpackPlugin({
+			patterns: [{ from: 'src/multithreading/workers/node/worker.js', to: 'dist' }]
+		})
+	],
+	externals: ['child_process', 'os'],
+	node: {
+		__dirname: false
+	}
+};
